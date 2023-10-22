ceede9f4660a247b9c2dc349b9dd46ebb92be6d7
Mukeswar Shah <mukeswar.github@gmail.com>
2023-10-22 15:03:23 +0545

    crested signup api route

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose.connect(process.env.MONGO)
  .then(()=>{
@@ -11,8 +12,11 @@ mongoose.connect(process.env.MONGO)
   console.log(err);
  })
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!');
 });
 
 app.use("/Api/user", userRouter);
+app.use("/Api/auth", authRouter);
--- Api/models/user.model.js
+++ Api/models/user.model.js
@@ -6,13 +6,18 @@ const userSchema = new mongoose.Schema({
   required:true,
   unique:true,
   },
-  username:{
+  email:{
     type: String,
     required:true,
+    unique:true,
 
     },
+    password:{
+      type: String,
+      required:true,
   
-
+      }
+  
 },{timestamps:true});
 
 const User = mongoose.model('User',userSchema);
